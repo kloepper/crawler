8723dc7d0e7bec5c92cc70425b47b1160133ed61
Kyle Kloepper <kkloepper@cloudflare.com>
2020-03-14 14:29:19 -0500

    List unique links from a single page.

--- crawl.sh
+++ crawl.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-lynx -dump -listonly https://www.rescale.com
+
+function get_unique_links() {
+    local url=$1
+    lynx -dump -listonly $url | # Use lynx text browser to list all links.
+        grep -o "https*://.*$" |  # Extract only the URL portion of the lynx list output.
+        awk '!x[$0]++' # Filter out duplicate links.
+}
+
+FIRST_URL=$1
+if [ -z "$FIRST_URL" ]
+then
+    echo "Please specify starting URL."
+    echo "Usage:"
+    echo "crawl.sh <URL_START>"
+    exit 1
+fi
+
+get_unique_links $FIRST_URL
+
