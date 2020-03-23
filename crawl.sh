d7651ae43407fee81e66d1c32fad910428b133f9
Kyle Kloepper <kkloepper@cloudflare.com>
2020-03-22 21:29:28 -0500

    Allow bash script to resume.

--- crawl.sh
+++ crawl.sh
@@ -16,18 +16,30 @@ then
     exit 1
 fi
 
-allUrls=($FIRST_URL)
-INDEX=0
-while [ $INDEX -lt ${#allUrls[@]} ]; do
-    CURRENT_URL=${allUrls[$INDEX]}
-    echo $CURRENT_URL
+DISPLAY_FILE="crawl_$FIRST_URL.display"
+OUTPUT_FILE="crawl_$FIRST_URL.urls"
+INDEX_FILE="crawl_$FIRST_URL.index"
+
+if [ -f "$INDEX_FILE" ]; then
+    INDEX=`cat "$INDEX_FILE"`
+else
+    INDEX=1
+    echo $FIRST_URL > "$OUTPUT_FILE"
+fi
+
+while true; do
+    CURRENT_URL=`sed -n "$INDEX,1p" "$OUTPUT_FILE"`
+    echo $CURRENT_URL | tee -a "$DISPLAY_FILE"
     for URL in `get_unique_links $CURRENT_URL`; do
-        echo "  $URL"
-        allUrls+=($URL)
+        echo "  $URL" | tee -a "$DISPLAY_FILE"
+        echo $URL >> "$OUTPUT_FILE"
     done
 
     # Remove duplicate URLs
-    allUrls=($(echo "${allUrls[@]}" | tr ' ' '\n' | awk '!x[$0]++' | tr '\n' ' '))
+    TEMP_FILE="$OUTPUT_FILE.filtered"
+    cat "$OUTPUT_FILE" | awk '!x[$0]++' > "$TEMP_FILE"
+    mv "$TEMP_FILE" "$OUTPUT_FILE"
 
     INDEX=$(($INDEX+1))
+    echo $INDEX > "$INDEX_FILE"
 done
